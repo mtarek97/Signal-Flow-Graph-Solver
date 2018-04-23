0b2213dbce4f7ea6e76ebd86d53f7bb20b8ecaaa
ma7moud_tarek97 <ma7moud.tarek97@gmail.com>
2018-04-23 23:20:15 +0200

    finished most of drag & drop mode

--- js/GUI.js
+++ js/GUI.js
@@ -1,185 +1,163 @@
-var stageWidth = 1000;
-var stageHeight = 600;
+var nodesCount = 3;
 
-// first we need to create a stage
-var stage = new Konva.Stage({
-    container: 'container',
-    width: stageWidth,
-    height: stageHeight
-});
-
-// then create layer
-var layer = new Konva.Layer();
-
-// create our shape
-var circle = new Konva.Circle({
-    // center coordiantes
-    x: stage.getWidth() / 2,
-    y: stage.getHeight() / 2,
-    radius: 70,
-    fill: 'red',
-    stroke: 'black',
-    strokeWidth: 4,
-    shadowOffsetX: 20,
-    shadowOffsetY: 25,
-    shadowBlur: 40,
-    opacity: 0.5,
-    draggable: 'true'
-});
-
-circle.to({
-    fill: 'green'
-});
-
-var simpleText = new Konva.Text({
-    x: stage.getWidth() / 2,
-    y: 15,
-    text: 'Simple Text',
-    fontSize: 30,
-    fontFamily: 'Calibri',
-    fill: 'green'
-});
-
-// to align text in the middle of the screen, we can set the
-// shape offset to the center of the text shape after instantiating it
-simpleText.setOffset({
-    x: simpleText.getWidth() / 2
-});
-
-// tooltip
-var tooltip = new Konva.Label({
-    x: 170,
-    y: 75,
-    opacity: 0.75
-});
-
-var arc = new Konva.Arc({
-    x: stage.getWidth() / 2,
-    y: stage.getHeight() / 2,
-    innerRadius: 40,
-    outerRadius: 40,
-    angle: 180,
-    fill: 'yellow',
-    stroke: 'black',
-    strokeWidth: 4,
-    draggable: 'true'
-});
-
-
-/*
- * create our branch shape by defining a
- * drawing function which draws a branch (arc with 3 points)
- */
-
-var PI2 = Math.PI * 2;
-
-var initial = {
-    x: 20,
-    y: 40
-};
-var median = {
-    x: 40,
-    y: 33
-};
-var final = {
-    x: 180,
-    y: 40
-};
-
-var controlX = 2 * median.x - initial.x / 2 - final.x / 2;
-var controlY = 2 * median.y - initial.y / 2 - final.y / 2;
-
-var branch = new Konva.Shape({
-    sceneFunc: function (context) {
-
-        context.beginPath();
-        context.arc(initial.x, initial.y, 4, 0, PI2);
-        context.closePath();
-        context.moveTo(median.x, median.y);
-        context.arc(median.x, median.y, 4, 0, PI2);
-        context.closePath();
-        context.moveTo(final.x, final.y);
-        context.arc(final.x, final.y, 4, 0, PI2);
-        context.closePath();
-        context.fill();
-
-        context.strokeStyle = 'red';
-        context.beginPath();
-        context.moveTo(initial.x, initial.y);
-        context.quadraticCurveTo(controlX, controlY, final.x, final.y);
-        context.stroke();
-    },
-    fill: '#00D2FF',
-    stroke: 'black',
-    strokeWidth: 4,
-    draggable: 'true'
-});
-
-var fromX = 50;
-var fromY = 50;
-var toX = 100;
-var toY = 50;
-var cpX = 75;
-var cpY = 100;
-
-var triangle = new Konva.Shape({
-    sceneFunc: function (context) {
-
-        context.beginPath();
-        context.moveTo(fromX, fromY);
-        context.quadraticCurveTo(cpX, cpY, toX, toY);
-        context.stroke();
-        context.closePath();
-
-
-    },
-    fill: '#00D2FF',
-    stroke: 'black',
-    strokeWidth: 4,
-    draggable: 'true'
-});
-
-// add the triangle shape to the layer
-layer.add(triangle);
-
-
-
-// add the triangle shape to the layer
-layer.add(branch);
-
-
-
-
-// add the shape to the layer
-layer.add(arc);
-
-
-// add the shape to the layer
-layer.add(circle);
-layer.add(simpleText);
-
-// add the layer to the stage
-stage.add(layer);
-
-
-function fitStageIntoParentContainer() {
-    var container = document.querySelector('#container');
-
-    // now we need to fit stage into parent
-    var containerWidth = container.offsetWidth;
-    // to do this we need to scale the stage
-    var scale = containerWidth / stageWidth;
-
-
-    stage.width(stageWidth * scale);
-    stage.height(stageHeight * scale);
-    stage.scale({
-        x: scale,
-        y: scale
-    });
-    stage.draw();
+function resizeGraph() {
+    var container = cy.container();
+    container.style.width = '100%';
+    container.style.height = '100%';
+    cy.resize();
+    cy.fit();
+    var currentWidth = container.clientWidth;
+    var currentHeight = container.clientWidth;
+    console.log(currentWidth + "   " + currentWidth);
 }
 
-fitStageIntoParentContainer();
+function addBranch() {
+    alert("addBranch: not working yet");
+}
+
+function addNode() {
+    nodesCount++;
+    var newNode = {
+        group: "nodes",
+        data: {
+            href: 'http://cytoscape.org'
+        }
+    };
+    newNode.data.id = nodesCount.toString();
+    newNode.data.name = nodesCount.toString();
+    console.log(newNode.name);
+    cy.add(newNode);
+    resizeGraph();
+}
+
+
+function remove() {
+    cy.one('tap', 'node', function (evt) {
+        var node = evt.target;
+        cy.remove(node);
+        console.log("node " + node.id() + " has been removed");
+    });
+}
+
+
+function printAllnode() {
+    // print all the ids of the nodes in the graph
+    cy.nodes().forEach(function (ele) {
+        console.log(ele.id());
+    });
+}
+
+
+function clearAll() {
+    cy.nodes().forEach(function (ele) {
+        cy.remove(ele);
+    });
+    console.log("graph has been destroyed!");
+}
+
+var cy = cytoscape({
+    container: document.getElementById('cy'),
+
+    boxSelectionEnabled: false,
+    autounselectify: true,
+    maxZoom: 5,
+    zoomingEnabled: true,
+    userZoomingEnabled: true,
+    selectionType: 'single',
+
+    style: cytoscape.stylesheet()
+        .selector('node')
+        .css({
+            'content': 'data(name)',
+            'text-valign': 'center',
+            'color': 'white',
+            'text-outline-width': 2,
+            'text-outline-color': '#1970fc',
+            'background-color': '#1970fc',
+        })
+        .selector(':selected')
+        .css({
+            'border-width': 50,
+            'border-color': '#333',
+            'background-color': 'black',
+            'line-color': 'black',
+            'target-arrow-color': 'black',
+            'source-arrow-color': 'black',
+            'text-outline-color': 'black'
+        })
+        .selector('edge')
+        .css({
+            'content': 'data(name)',
+            'text-valign': 'center',
+            'color': 'white',
+            'text-outline-width': 2,
+            'text-outline-color': '#6FB1FC',
+            'curve-style': 'unbundled-bezier',
+            'target-arrow-shape': 'triangle',
+            'line-color': '#6FB1FC',
+            'target-arrow-color': '#6FB1FC'
+        }),
+
+    elements: {
+        nodes: [
+            {
+                data: {
+                    id: '0',
+                    name: 'Labib',
+                }
+            },
+            {
+                data: {
+                    id: '2',
+                    name: 'A',
+                }
+            },
+            {
+                data: {
+                    id: '1',
+                    name: 'B',
+                }
+            }
+
+    ],
+        edges: [
+            {
+                data: {
+                    id: '6',
+                    source: '0',
+                    target: '2',
+                    name: '245'
+                }
+            }
+            , {
+                data: {
+                    id: '7',
+                    source: '1',
+                    target: '2',
+                    name: '6'
+                }
+            },
+            {
+                data: {
+                    id: '8',
+                    source: '2',
+                    target: '0',
+                    name: '456'
+                }
+            }
+    ]
+    },
+
+    layout: {
+        name: 'grid'
+    }
+});
+
+console.log("hey");
+
+
+
+resizeGraph();
 // adapt the stage on any window resize
-window.addEventListener('resize', fitStageIntoParentContainer);
+window.addEventListener('resize', resizeGraph);
+printAllnode();
